5ad20a048cda3683808ae65789fe837821166370
ASHMEET555 <sandhuashmeet40@gmail.com>
2025-11-01 11:22:25 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split  
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig 
 
 
 # where i have to store the data 
@@ -24,7 +26,8 @@ class DataIngestion:
     def initiate_data_ingestion(self): # if data is stored in some databases the code to read it 
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv("notebook\data\stud.csv") # change this line and able to read from api or mongo db
+            df = pd.read_csv("notebook\\data\\stud.csv")
+ # change this line and able to read from api or mongo
             logging.info("Read the dataset as dataframe")
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
@@ -50,4 +53,8 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+    
